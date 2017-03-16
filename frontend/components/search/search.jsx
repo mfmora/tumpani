f563e1883eb090b43a43dd14a3f5425e8bc2a088
Fernanda <fernandamora88@gmail.com>
2017-03-15 21:28:24 -0700

    set initial buttons with material design style and hover

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,11 +3,23 @@ import { withRouter } from 'react-router';
 
 class Search extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this._handleClickSearch = this._handleClickSearch.bind(this);
+  }
+
+  _handleClickSearch(e) {
+    e.preventDefault();
+  }
+
   render () {
     return (
       <container id="search-bar">
         <i className="material-icons">search</i>
-        <input placeholder="Search" id="search-input"></input>
+        <input onClick={ this._handleClickSearch }
+               placeholder="Search"
+               id="search-input" />
       </container>
     );
   }
