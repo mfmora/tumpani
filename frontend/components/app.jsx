c5e8e4dd442d926e6c612b10e7147cb7e0ee9457
Fernanda <fernandamora88@gmail.com>
2017-07-09 12:54:27 -0700

    change home page message to Discover all sf attractions in one app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,7 +4,7 @@ import SessionFormContainer from './session_form/session_form_container';
 const App = ({ children }) => (
   <section id="main">
     <h1>Tumpani</h1>
-    <h3>All SF attractions in one app</h3>
+    <h3>Discover All SF Attractions in one App</h3>
     <SessionFormContainer />
     { children }
   </section>
